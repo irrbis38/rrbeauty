f6790b99b2b66e0469875de7086a5cd09aa0f8fe
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2023-09-06 08:58:02 +0300

    deploy main page on gh-pages

--- sources/src/js/_libs_concat.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = [
-  // comma separated
-  // "./src/libs/jquery/dist/jquery.min.js"
-  "./src/libs/swiper/swiper-bundle.min.js",
-  "./src/libs/splide/splide.min.js",
-  "./src/libs/splide/splide-extension-auto-scroll.min.js",
-  "./src/libs/choices/choices.min.js",
-];
