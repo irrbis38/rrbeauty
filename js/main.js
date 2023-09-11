bd97ac1443bc8aaf4ea847b1875a35972587b036
Vasil Ilyuhin <v.ilyuhin@gmail.com>
2023-09-11 21:10:43 +0300

    update gh-pages

--- js/main.js
+++ js/main.js
@@ -1,18 +1,64 @@
 document.addEventListener("DOMContentLoaded", function (event) {
   doHeaderInit();
   doPanelInit();
+  doToggleFooterAccordion();
+
+  // init GSAP ScrollTrigger
+  gsap.registerPlugin(ScrollTrigger);
 
   // main page
 
   const intro_section = document.querySelector(".intro");
   if (intro_section) {
     doIntroSectionInit();
-    doInitMap();
+    if (typeof ymaps !== "undefined" && ymaps !== null) {
+      doInitMap();
+    }
     doRemoveMapOverlayByClick();
     doInitMapStoresSelect();
     doAddMapStoresListener();
     doHideMapDescription();
-    doToggleFooterAccordion();
+    doToggleFavoritesIcons();
+    doParallaxPromotionsSection();
+  }
+
+  // catalog page
+  const catalog_page = document.querySelector(".catalog-page");
+
+  if (catalog_page) {
+    doToggleFavoritesIcons();
+    doSortMenuLogic();
+    doFiltersMenuLogic();
+    handleAllInputRange();
+  }
+
+  // catalog brand page
+  const catalog_brand_page = document.querySelector(".catalog-brand-page");
+
+  if (catalog_brand_page) {
+    doToggleFavoritesIcons();
+    doSortMenuLogic();
+    doFiltersMenuLogic();
+    handleAllInputRange();
+    doToggleBrandDescription();
+  }
+
+  // catalog favorites page
+  const catalog_favorites_page = document.querySelector(
+    ".catalog-favorites-page"
+  );
+
+  if (catalog_favorites_page) {
+    doToggleFavoritesIcons();
+  }
+
+  // catalog favorites page
+  const catalog_search_result_page = document.querySelector(
+    ".catalog-search-result-page"
+  );
+
+  if (catalog_search_result_page) {
+    doToggleFavoritesIcons();
   }
 
   // ====== END OF DOMContentLoaded LISTENERS ========
@@ -156,9 +202,9 @@ function doIntroSectionInit() {
       delay: 5000,
       disableOnInteraction: false,
     },
-    hashNavigation: {
-      watchState: true,
-    },
+    // hashNavigation: {
+    //   watchState: true,
+    // },
 
     // pagination
     pagination: {
@@ -182,9 +228,9 @@ function doIntroSectionInit() {
       delay: 5000,
       disableOnInteraction: false,
     },
-    hashNavigation: {
-      watchState: true,
-    },
+    // hashNavigation: {
+    //   watchState: true,
+    // },
 
     // pagination
     pagination: {
@@ -215,9 +261,9 @@ function doIntroSectionInit() {
       delay: 5000,
       disableOnInteraction: false,
     },
-    hashNavigation: {
-      watchState: true,
-    },
+    // hashNavigation: {
+    //   watchState: true,
+    // },
 
     // pagination
     pagination: {
@@ -266,19 +312,6 @@ function doIntroSectionInit() {
     }
   });
 
-  // addToFavorite button
-
-  const goodsCard_addToFavorites_buttons = document.querySelectorAll(
-    ".goodsCard__addToFavorites"
-  );
-
-  goodsCard_addToFavorites_buttons.forEach((btn) =>
-    btn.addEventListener("click", (e) => {
-      const button = e.target.closest(".goodsCard__addToFavorites");
-      button.classList.toggle("addedToFavorites");
-    })
-  );
-
   // init autoscroll blocks
 
   const brandsSection_autoscroll = document.querySelector(
@@ -289,25 +322,85 @@ function doIntroSectionInit() {
   );
   const map_autoscroll = document.querySelector(".map__autoscroll");
 
-  [brandsSection_autoscroll, promotionsSection_autoscroll].forEach((slider) => {
+  [brandsSection_autoscroll].forEach((slider) => {
     new Splide(slider, {
       type: "loop",
       arrows: false,
       pagination: false,
-      perPage: 2,
-      gap: "120px",
+      perPage: 3,
+      gap: "800px",
       breakpoints: {
+        1700: {
+          gap: "900px",
+        },
+        1600: {
+          gap: "400px",
+        },
+        1400: {
+          gap: "600px",
+        },
+        992: {
+          gap: "200px",
+          perPage: 2,
+        },
         767: {
           perPage: 3,
-          gap: "200px",
+          gap: "100px",
+        },
+        650: {
+          gap: "150px",
         },
         575: {
           perPage: 2,
+          gap: "100px",
+        },
+        400: {
+          gap: "170px",
         },
       },
 
       autoScroll: {
-        speed: 0.5,
+        speed: 1,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+      },
+    }).mount(window.splide.Extensions);
+  });
+
+  [promotionsSection_autoscroll].forEach((slider) => {
+    new Splide(slider, {
+      type: "loop",
+      arrows: false,
+      pagination: false,
+      perPage: 3,
+      gap: "400px",
+      breakpoints: {
+        1700: {
+          gap: "600px",
+        },
+        1600: {
+          gap: "200px",
+        },
+        1300: {
+          gap: "300px",
+        },
+        1150: {
+          gap: "500px",
+        },
+        992: {
+          gap: "350px",
+        },
+        767: {
+          gap: "60px",
+        },
+        575: {
+          // perPage: 2,
+          gap: "230px",
+        },
+      },
+
+      autoScroll: {
+        speed: 1,
         pauseOnHover: false,
         pauseOnFocus: false,
       },
@@ -319,15 +412,32 @@ function doIntroSectionInit() {
       type: "loop",
       arrows: false,
       pagination: false,
-      perPage: 1,
-      gap: "120px",
+      perPage: 2,
+      gap: "1700px",
       breakpoints: {
-        575: {
-          gap: "200px",
+        1600: {
+          gap: "1000px",
+        },
+        1400: {
+          gap: "1100px",
+        },
+        1300: {
+          gap: "1200px",
+        },
+        1100: {
+          // perPage: 1,
+          gap: "1250px",
+        },
+        992: {
+          // perPage: 1,
+          gap: "950px",
+        },
+        767: {
+          gap: "500px",
         },
       },
       autoScroll: {
-        speed: 0.5,
+        speed: 1,
         pauseOnHover: false,
         pauseOnFocus: false,
       },
@@ -338,36 +448,39 @@ function doIntroSectionInit() {
 // init map on main page
 
 function doInitMap() {
-  const mark_link = "/images/map-current-mark.svg";
+  const mark_link = "images/map-current-mark.svg";
   function init() {
     let center = [51.158562572612595, 71.43921449999996];
-    let map = new ymaps.Map("map-section-wrapper", {
-      center: center,
-      zoom: 17,
-    });
+    if (ymaps) {
+      let map = new ymaps.Map("map-section-wrapper", {
+        center: center,
+        zoom: 17,
+      });
 
-    let mark = new ymaps.Placemark(
-      center,
-      {},
-      {
-        iconLayout: "default#image",
-        iconImageHref: mark_link,
-        iconImageSize: [40, 40],
-        iconImageOffset: [-15, -5],
-      }
-    );
+      let mark = new ymaps.Placemark(
+        center,
+        {},
+        {
+          iconLayout: "default#image",
+          iconImageHref: mark_link,
+          iconImageSize: [40, 40],
+          iconImageOffset: [-15, -5],
+        }
+      );
 
-    map.controls.remove("geolocationControl"); // удаляем геолокацию
-    map.controls.remove("searchControl"); // удаляем поиск
-    map.controls.remove("trafficControl"); // удаляем контроль трафика
-    map.controls.remove("typeSelector"); // удаляем тип
-    map.controls.remove("fullscreenControl"); // удаляем кнопку перехода в полноэкранный режим
-    map.controls.remove("zoomControl"); // удаляем контрол зуммирования
-    map.controls.remove("rulerControl"); // удаляем контрол правил
-    //map.behaviors.disable(["scrollZoom"]); // отключаем скролл карты (опционально)
+      map.controls.remove("geolocationControl"); // удаляем геолокацию
+      map.controls.remove("searchControl"); // удаляем поиск
+      map.controls.remove("trafficControl"); // удаляем контроль трафика
+      map.controls.remove("typeSelector"); // удаляем тип
+      map.controls.remove("fullscreenControl"); // удаляем кнопку перехода в полноэкранный режим
+      map.controls.remove("zoomControl"); // удаляем контрол зуммирования
+      map.controls.remove("rulerControl"); // удаляем контрол правил
+      //map.behaviors.disable(["scrollZoom"]); // отключаем скролл карты (опционально)
 
-    map.geoObjects.add(mark);
+      map.geoObjects.add(mark);
+    }
   }
+
   ymaps.ready(init);
 }
 
@@ -423,7 +536,6 @@ function doToggleFooterAccordion() {
   const mq575 = window.matchMedia("(max-width: 575px)");
 
   mq575.addEventListener("change", (e) => {
-    // console.log("asdfasg;qweoqwhe");
     if (e.matches) {
       doAddListenersToFooterAccordions();
     } else {
@@ -473,5 +585,373 @@ function doRemoveListenersToFooterAccordions() {
 
     column.classList.remove("active");
     wrapper.style.maxHeight = null;
+  });
+}
+
+// toggle addToFavorites buttons
+
+function doToggleFavoritesIcons() {
+  // addToFavorite button
+
+  const goodsCard_addToFavorites_buttons = document.querySelectorAll(
+    ".goodsCard__addToFavorites"
+  );
+
+  goodsCard_addToFavorites_buttons.forEach((btn) =>
+    btn.addEventListener("click", (e) => {
+      const button = e.target.closest(".goodsCard__item");
+      button.classList.toggle("addedToFavorites");
+    })
+  );
+}
+
+// parallax for 'discountedProducts' on main page
+
+function doParallaxPromotionsSection() {
+  const discounted_products = document.querySelector(".discountedProducts");
+  const goods_card_items =
+    discounted_products.querySelectorAll(".goodsCard__item");
+  const TL = gsap.timeline();
+
+  TL.from(".textLline__first", {
+    x: "200px",
+    scrollTrigger: {
+      trigger: ".discountedProducts",
+      // markers: true,
+      start: "top bottom",
+      end: "bottom bottom",
+      scrub: true,
+    },
+  })
+    .from(
+      ".textLline__second",
+      {
+        x: "-200px",
+        scrollTrigger: {
+          trigger: ".discountedProducts",
+          // markers: true,
+          start: "top bottom",
+          end: "bottom bottom",
+          scrub: true,
+        },
+      },
+      0
+    )
+    .from(
+      goods_card_items[0],
+      {
+        y: "-232px",
+        ease: Power2.easeOut,
+        scrollTrigger: {
+          trigger: ".goodsCard__wrapper",
+          // markers: true,
+          start: "+=200px bottom",
+          end: "bottom+=400px bottom",
+          scrub: true,
+        },
+      },
+      0
+    )
+    .from(
+      goods_card_items[2],
+      {
+        y: "-98px",
+        ease: Power2.easeOut,
+        scrollTrigger: {
+          trigger: ".goodsCard__wrapper",
+          // markers: true,
+          start: "+=200px bottom",
+          end: "bottom+=200px bottom",
+          scrub: true,
+        },
+      },
+      0
+    );
+}
+
+// sort menu logic
+
+function doSortMenuLogic() {
+  const body = document.body;
+  const options_sort_toggle_btn = document.querySelector(
+    ".options__sortToggleBtn"
+  );
+  const options_overlay = document.querySelector(".options__overlay");
+  const options_buttons = Array.from(
+    document.querySelectorAll(".options__sortList li button")
+  );
+  const elements = [].concat(options_overlay, ...options_buttons);
+  const options_current = document.querySelector(".options__current");
+
+  // init functions
+  doOpenSortMenu();
+
+  // show sort menu
+  function doOpenSortMenu() {
+    options_sort_toggle_btn.addEventListener("click", () => {
+      body.classList.add("sort-list-open");
+      elements.forEach((el) =>
+        el.addEventListener("click", handleHideSortMenu)
+      );
+      doChangeCurrentSortOption();
+    });
+  }
+
+  // hide sort menu
+  function handleHideSortMenu() {
+    body.classList.remove("sort-list-open");
+    elements.forEach((el) =>
+      el.removeEventListener("click", handleHideSortMenu)
+    );
+  }
+
+  // change current sort option
+  function doChangeCurrentSortOption() {
+    options_buttons.forEach((btn) =>
+      btn.addEventListener("click", handleSortButtons)
+    );
+  }
+
+  function handleSortButtons(e) {
+    let currentSortType = options_sort_toggle_btn.dataset.sortType;
+    const clickedSortButton = e.target.closest(".options__sortList button");
+    let newSortType = clickedSortButton.dataset.sortType;
+
+    if (currentSortType !== newSortType) {
+      // place to send a request when changing sort type
+
+      options_current.textContent = clickedSortButton.children[0].textContent;
+      options_sort_toggle_btn.dataset.sortType = newSortType;
+    }
+  }
+}
+
+// filters menu logic
+
+function doFiltersMenuLogic() {
+  const options_nav = document.querySelector(".options__nav");
+  const options_filters_toggle_btn = document.querySelector(
+    ".options__filtersToggleBtn"
+  );
+  const filters_buttons = Array.from(
+    document.querySelectorAll(".filters__button")
+  );
+  const filters = document.querySelector(".filters");
+
+  options_filters_toggle_btn.addEventListener("click", () => {
+    options_nav.classList.toggle("filters-opened");
+    filters.classList.toggle("filters-opened");
+  });
+
+  filters_buttons.forEach((btn) =>
+    btn.addEventListener("click", () => {
+      const filter_block = btn.closest(".filters__block");
+      const filter_wrapper = filter_block.querySelector(".filters__wrapper");
+
+      // toggle filters_list
+      filter_block.classList.toggle("filters-block-opened");
+
+      if (filter_block.classList.contains("filters-block-opened")) {
+        filter_wrapper.style.maxHeight = filter_wrapper.scrollHeight + "px";
+      } else {
+        filter_wrapper.style.maxHeight = null;
+      }
+    })
+  );
+}
+
+function handleAllInputRange() {
+  // handle all input range in filters
+
+  const filters_range_inputs = document.querySelectorAll(".filters__range");
+
+  filters_range_inputs.forEach((rangeInput) =>
+    rangeInput.addEventListener("input", doHandleRangeInputs)
+  );
+
+  function doHandleRangeInputs(e) {
+    // get current inputs - min and max
+    let minInputRange, maxInputRange;
+    const isMinInputRange = e.target.classList.contains("filters__inputMin");
+    const isMaxInputRange = e.target.classList.contains("filters__inputMax");
+    if (isMinInputRange) {
+      minInputRange = e.target;
+      maxInputRange = minInputRange.nextElementSibling;
+    } else if (isMaxInputRange) {
+      maxInputRange = e.target;
+      minInputRange = maxInputRange.previousElementSibling;
+    }
+
+    // get elements
+    const filter_wrapper = minInputRange.closest(".filters__wrapper");
+    const track = filter_wrapper.querySelector(".filters__track");
+    const min = filter_wrapper.querySelector(".filters__valueMin");
+    const max = filter_wrapper.querySelector(".filters__valueMax");
+
+    // convert values to number
+    let minValue = parseInt(minInputRange.value);
+    let maxValue = parseInt(maxInputRange.value);
+
+    // minDifference is the minimum value by which min and max can converge
+    let minDifference;
+
+    if (parseInt(maxInputRange.max) - parseInt(minInputRange.min) <= 10) {
+      minDifference = 1;
+    } else if (
+      parseInt(maxInputRange.max) - parseInt(minInputRange.min) <=
+      100
+    ) {
+      minDifference = 10;
+    } else {
+      minDifference = 50;
+    }
+
+    // if the input cannot be moved further
+    if (maxValue - minValue < minDifference) {
+      if (isMinInputRange) {
+        minInputRange.value = maxValue - minDifference;
+      } else {
+        maxInputRange.value = minValue + minDifference;
+      }
+    } else {
+      // otherwise calculates the value of the  two curret inputs and displays their values
+      min.value = `${minValue}`;
+      max.value = `${maxValue}`;
+      let left =
+        ((minValue - minInputRange.min) * 100) /
+          (minInputRange.max - minInputRange.min) +
+        "%";
+      let right =
+        ((maxInputRange.max - maxValue) * 100) /
+          (maxInputRange.max - maxInputRange.min) +
+        "%";
+      track.style.left = left;
+      track.style.right = right;
+    }
+  }
+
+  // handle all min and max value
+  const filters_value_min = Array.from(
+    document.querySelectorAll(".filters__valueMin")
+  );
+
+  const filters_value_max = Array.from(
+    document.querySelectorAll(".filters__valueMax")
+  );
+
+  filters_value_min.forEach((input) =>
+    input.addEventListener("change", handleInputMinMax)
+  );
+
+  filters_value_max.forEach((input) =>
+    input.addEventListener("change", handleInputMinMax)
+  );
+
+  filters_value_min.forEach((input) =>
+    input.addEventListener("blur", handleInputMinMax)
+  );
+
+  filters_value_max.forEach((input) =>
+    input.addEventListener("blur", handleInputMinMax)
+  );
+
+  function handleInputMinMax(e) {
+    const input = e.target;
+    const filters_wrapper = input.closest(".filters__wrapper");
+    const input_min_range = filters_wrapper.querySelector(".filters__inputMin");
+    const input_max_range = filters_wrapper.querySelector(".filters__inputMax");
+    let input_range = null;
+
+    const isMinFiltersInput = input.classList.contains("filters__valueMin");
+    const isMaxFiltersInput = input.classList.contains("filters__valueMax");
+
+    const track = filters_wrapper.querySelector(".filters__track");
+
+    // minDifference is the minimum value by which min and max can converge
+    let minDifference;
+
+    if (input_max_range.max - input_min_range.min <= 10) {
+      minDifference = 1;
+    } else if (input_max_range.max - input_min_range.min <= 100) {
+      minDifference = 10;
+    } else {
+      minDifference = 50;
+    }
+
+    let value = parseInt(input.value);
+
+    // for min
+    if (isMinFiltersInput) {
+      if (value < input.min || Number.isNaN(value)) {
+        // input.value = parseInt(input.min);
+        input.value = "";
+        input_min_range.value = parseInt(input.min);
+        track.style.left = 0;
+      } else if (value > input_max_range.value - minDifference) {
+        input.value = parseInt(input_max_range.value) - minDifference;
+        input_min_range.value = parseInt(input_max_range.value) - minDifference;
+        doSetTrackLeft();
+      } else {
+        input.value = value;
+        input_min_range.value = value;
+        doSetTrackLeft();
+      }
+    }
+
+    function doSetTrackLeft() {
+      let left =
+        ((value - input_min_range.min) * 100) /
+          (input_min_range.max - input_min_range.min) +
+        "%";
+      track.style.left = left;
+    }
+
+    // for max
+    if (isMaxFiltersInput) {
+      if (value > parseInt(input.max) || Number.isNaN(value)) {
+        input.value = "";
+        input_max_range.value = parseInt(input.max);
+        track.style.right = "0";
+        console;
+      } else if (value < parseInt(input_min_range.value) + minDifference) {
+        input.value = parseInt(input_min_range.value) + minDifference;
+        input_max_range.value = parseInt(input_min_range.value) + minDifference;
+        doSetTrackRight();
+      } else {
+        input.value = value;
+        input_max_range.value = value;
+        doSetTrackRight();
+      }
+    }
+
+    function doSetTrackRight() {
+      let right =
+        ((input_max_range.max - value) * 100) /
+          (input_max_range.max - input_max_range.min) +
+        "%";
+      track.style.right = right;
+    }
+  }
+}
+
+// accordion for brandDescription on catalog-brand-page
+function doToggleBrandDescription() {
+  const brand_description = document.querySelector(".brandDescription");
+  const brand_description_title = document.querySelector(
+    ".brandDescription__title"
+  );
+  const brand_description_info = document.querySelector(
+    ".brandDescription__info"
+  );
+
+  brand_description_title.addEventListener("click", () => {
+    brand_description.classList.toggle("brand-description-opened");
+
+    if (brand_description.classList.contains("brand-description-opened")) {
+      brand_description_info.style.maxHeight =
+        brand_description_info.scrollHeight + "px";
+    } else {
+      brand_description_info.style.maxHeight = null;
+    }
   });
 }
