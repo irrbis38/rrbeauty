a196a9a2f1f161287a09275c61bb1aedb6471a92
Vasil Ilyuhin <v.ilyuhin@gmail.com>
2023-09-20 13:22:50 +0300

    refactor: add swiper, remove slite, fix catalogShow__btn

--- sources/src/js/_libs_concat.cjs
+++ sources/src/js/_libs_concat.cjs
@@ -1,9 +1,9 @@
 module.exports = [
   // comma separated
   // "./src/libs/jquery/dist/jquery.min.js"
-  // "./src/libs/swiper/swiper-bundle.min.js",
-  "./src/libs/splide/splide.min.js",
-  "./src/libs/splide/splide-extension-auto-scroll.min.js",
+  "./src/libs/swiper/swiper-bundle.min.js",
+  // "./src/libs/splide/splide.min.js",
+  // "./src/libs/splide/splide-extension-auto-scroll.min.js",
   "./src/libs/choices/choices.min.js",
   "./src/libs/gsap/gsap.min.js",
   "./src/libs/gsap/ScrollTrigger.min.js",
